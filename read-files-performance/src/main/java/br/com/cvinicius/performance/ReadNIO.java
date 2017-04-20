3f31b9f6442b29ff66512aa7c6ad444ffe22cf97
Caio <caio@wisetelecom.com.br>
2017-04-20 18:48:44 -0300

    Projeto para avaliar a performance na leitura de arquivos com Java.

---
+++ read-files-performance/src/main/java/br/com/cvinicius/performance/ReadNIO.java
@@ -0,0 +1,36 @@
+package br.com.cvinicius.performance;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Leitura com Java NIO.
+ * 
+ * @author cvinicius
+ * @since 20/04/2017
+ * @version 1.0
+ */
+public class ReadNIO{
+	
+	private static final Path FILE = Paths.get("files/performance.txt");
+	
+	public static void main(String[] args) 
+	throws Exception{
+		
+		long initial = System.currentTimeMillis();
+		
+		try{
+			System.out.println(" >>>> Inicio da execução.");
+			
+			Files.readAllLines(FILE)
+				 .stream()
+				 .forEach(str -> {});
+			
+		}finally{
+			long end = System.currentTimeMillis();
+			
+			System.out.println(String.format(" >>>> Tempo de execução: %d segundos.", ((end - initial) / 1000)));
+		}
+	}
+}
