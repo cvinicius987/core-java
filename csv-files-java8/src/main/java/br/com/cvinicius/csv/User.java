266298543257ef36609b380ce355059ddb447fcb
Caio <caio@wisetelecom.com.br>
2018-02-21 19:15:15 -0300

    Alterações

--- csv-files-java8/src/main/java/br/com/cvinicius/csv/User.java
+++ csv-files-java8/src/main/java/br/com/cvinicius/csv/User.java
@@ -1,42 +1,15 @@
 package br.com.cvinicius.csv;
 
-public class User {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
 
-	private final String columns = "{Name: %s, Cpf: %s, Age: %s, Phone: %s, Address: %s}";
+@Getter
+@ToString
+@AllArgsConstructor
+public class User {
 	
-	private String name, cpf, phone, address;
+	private String name, cpf;
 	private Integer age;
-	
-	public User(String name, String cpf, Integer age, String phone, String address){
-		this.name 	  	= name;
-		this.cpf 	  	= cpf;
-		this.age 	  	= age;
-		this.phone 		= phone;
-		this.address 	= address;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public String getCpf() {
-		return cpf;
-	}
-	
-	public String getPhone() {
-		return phone;
-	}
-	
-	public String getAddress() {
-		return address;
-	}
-	
-	public Integer getAge() {
-		return age;
-	}
-	
-	@Override
-	public String toString(){
-		return String.format(columns, this.name, this.cpf, this.age, this.phone, this.address);
-	}
+	private String phone, address;
 }
