41184f7d253737a1ab94a274a0366fe970dc4497
Caio <caio@wisetelecom.com.br>
2018-03-09 19:57:00 -0300

    Alterações para o desenvolvimento do tutorial no Blog.

--- csv-files-java8/src/main/java/br/com/cvinicius/csv/Csv.java
+++ csv-files-java8/src/main/java/br/com/cvinicius/csv/Csv.java
@@ -3,70 +3,50 @@ package br.com.cvinicius.csv;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Csv {
 	
-	private final String columns = "{Name: %s, Cpf: %s, Age: %s, Phone: %s, Address: %s}";
-	
-	/***
-	 * Leitura das colunas do arquivo.
-	 * 
-	 * @return List<String[]>
-	 * @throws IOException
-	 */
-	public List<String> getColumns() throws IOException{
+	public static void main(String[] args) 
+	throws IOException {
 		
-		return Files.lines(Paths.get("file.csv"))
-					.map(list -> list.split(";"))
-					.map(str -> String.format(columns, str[0], str[1], str[2], str[3], str[4]))
-					.collect(Collectors.toList());
-	}
-	
-	/***
-	 * Leitura das colunas do arquivo, desprezando a linha do titulo.
-	 * 
-	 * @return List<String[]>
-	 * @throws IOException
-	 */
-	public List<String> getColumnsWithoutFirstLine() throws IOException{
+		/*
+		 * Leitura do arquivo
+		 */
+		System.out.println("============ Leitura do arquivo: \n");
 		
-		return Files.lines(Paths.get("file.csv"))
-					.skip(1)
-					.map(list -> list.split(";"))
-					.map(str -> String.format(columns, str[0], str[1], str[2], str[3], str[4]))
-					.collect(Collectors.toList());
-	}
-	
-	/***
-	 * Leitura das colunas do arquivo, e transformando para objeto.
-	 * 
-	 * @return List<Usuario>
-	 * @throws IOException
-	 */
-	public List<User> getColumnsToObj() throws IOException{
+		Files.lines(Paths.get("file.csv"))
+			 .forEach(System.out::println);
 		
-		return Files.lines(Paths.get("file.csv"))
-					.skip(1)
-					.map(list -> list.split(";"))
-					.map(str -> new User(str[0], str[1], Integer.parseInt(str[2]), str[3], str[4]))
-					.collect(Collectors.toList());
-	}
-	
-	/***
-	 * Leitura das colunas do arquivo, e transformando para objeto.
-	 * 
-	 * @return List<Usuario>
-	 * @throws IOException
-	 */
-	public List<User> getUserAgeBigger30() throws IOException{
+		System.out.println("\n============ Leitura do arquivo sem cabecalho: \n");
 		
-		return Files.lines(Paths.get("file.csv"))
-					.skip(1)
-					.map(list -> list.split(";"))
-					.map(str -> new User(str[0], str[1], Integer.parseInt(str[2]), str[3], str[4]))
-					.filter(usr -> usr.getAge() > 30)
-					.collect(Collectors.toList());
+		/*
+		 * Leitura do arquivo sem cabecalho
+		 */
+		Files.lines(Paths.get("file.csv"))
+			 .skip(1)
+			 .forEach(System.out::println);
+		
+		/*
+		 * Conversão em um objeto do Tipo User
+		 */
+		System.out.println("\n============ Conversão em um objeto do Tipo User: \n");
+		
+		Files.lines(Paths.get("file.csv"))
+		 	 .skip(1)
+		 	 .map(line -> line.split(";"))
+		 	 .map(str -> new User(str[0], str[1], Integer.parseInt(str[2]), str[3], str[4]))
+		 	 .forEach(System.out::println);
+		
+		/*
+		 * Exibindo somente usuários com maiores de 30 anos
+		 */
+		System.out.println("\n============ Exibindo somente usuários com maiores de 30 anos: \n");
+		
+		Files.lines(Paths.get("file.csv"))
+				.skip(1)
+				.map(list -> list.split(";"))
+				.map(str -> new User(str[0], str[1], Integer.parseInt(str[2]), str[3], str[4]))
+				.filter(usr -> usr.getAge() > 30)
+				.forEach(System.out::println);
 	}
 }
